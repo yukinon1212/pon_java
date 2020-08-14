e65d3c4f422a3f82a40a2e54c95fb21161e44897
shodakasumi <kasupon9876@gmail.com>
2020-08-14 21:03:01 +0900

    [add] first commit ponpon

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("ブラックジャックへようこそ");
+        System.out.println("ゲームを開始します。");
+        Random rand = new Random();
+        int random_number = rand.nextInt(4);
+        System.out.println(random_number);
+        String mark[];
+        mark = new String[4];
+        mark[0] = "スペード";
+        mark[1] = "ハート";
+        mark[2] = "クラブ";
+        mark[3] = "ダイヤ";
+        System.out.println(mark[random_number]);
+        Random ran = new Random();
+        int random_kazu = ran.nextInt(13);
+        System.out.println(random_kazu +1);
+        System.out.println("あなたの引いたカードは" +mark[random_number] +"の" +(random_kazu +1) +"です。");
     }
 }
